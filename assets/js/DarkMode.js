bee7844ebf3381297e2d5648a2dc7ccbf50c5cb7
MrTechX <82182805+Mr-TechX@users.noreply.github.com>
2024-09-11 13:57:09 -0600

    DarkMode Function

---
+++ assets/js/DarkMode.js
@@ -0,0 +1,6 @@
+const DarkMode = document.querySelector('button');
+const body = document.querySelector('body');
+function DarkMode() {
+    // let body = document.getElementByTagName('body')[0];
+    body.style.backgroundColor = '#000000';
+}
